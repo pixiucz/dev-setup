1ac66f0e8d6f470689876d801086bf2af2ae43a1
Tomáš Ondruš <ondrus.tomas@outlook.com>
2017-01-26 14:28:19 +0100

    removed shell interactivity

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 # install apache
-apt install apache2
-apt install libapache2-mod-php7.0
+apt -y install apache2
+apt -y install libapache2-mod-php7.0
 a2enmod rewrite
 a2enmod php7.0
 
 # install mysql
-apt install mysql-server php7-mysql
+apt -y install mysql-server php7-mysql
 
 # install php
-apt install php7.0
+apt -y install php7.0
 
 # install composer
-apt install composer
+apt -y install composer
 
 # install npm
-apt install npm
+apt -y install npm
 ln -s /usr/bin/nodejs /usr/bin/node
 
-apt install php7.0-bcmath php7.0-fpm php7.0-mcrypt php7.0-bz2 php7.0-gd php7.0-mysql php7.0-soap php7.0-cgi php7.0-gmp php7.0-odbc php7.0-sqlite3 php7.0-cli php7.0-imap php7.0-opcache php7.0-sybase php7.0-common php7.0-interbase php7.0-pgsql php7.0-tidy php7.0-curl php7.0-intl php7.0-phpdbg php7.0-xml php7.0-dba php7.0-json php7.0-pspell php7.0-xmlrpc php7.0-dev php7.0-ldap php7.0-readline php7.0-xsl php7.0-enchant php7.0-mbstring php7.0-recode php7.0-zip
+apt -y install php7.0-bcmath php7.0-fpm php7.0-mcrypt php7.0-bz2 php7.0-gd php7.0-mysql php7.0-soap php7.0-cgi php7.0-gmp php7.0-odbc php7.0-sqlite3 php7.0-cli php7.0-imap php7.0-opcache php7.0-sybase php7.0-common php7.0-interbase php7.0-pgsql php7.0-tidy php7.0-curl php7.0-intl php7.0-phpdbg php7.0-xml php7.0-dba php7.0-json php7.0-pspell php7.0-xmlrpc php7.0-dev php7.0-ldap php7.0-readline php7.0-xsl php7.0-enchant php7.0-mbstring php7.0-recode php7.0-zip
 
 # install gulp
 npm install gulp-cli -g
