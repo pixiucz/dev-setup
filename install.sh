792562fa440d993618ecd3ef2c6b82cb850d9c08
Tomáš Ondruš <ondrus.tomas@outlook.com>
2017-02-27 16:13:39 +0100

    Added xdebug install

--- install.sh
+++ install.sh
@@ -17,7 +17,10 @@ apt -y install composer
 apt -y install npm
 ln -s /usr/bin/nodejs /usr/bin/node
 
-apt -y install php7.0-bcmath php7.0-fpm php7.0-mcrypt php7.0-bz2 php7.0-gd php7.0-mysql php7.0-soap php7.0-cgi php7.0-gmp php7.0-odbc php7.0-sqlite3 php7.0-cli php7.0-imap php7.0-opcache php7.0-sybase php7.0-common php7.0-interbase php7.0-pgsql php7.0-tidy php7.0-curl php7.0-intl php7.0-phpdbg php7.0-xml php7.0-dba php7.0-json php7.0-pspell php7.0-xmlrpc php7.0-dev php7.0-ldap php7.0-readline php7.0-xsl php7.0-enchant php7.0-mbstring php7.0-recode php7.0-zip
+apt -y install php7.0-bcmath php7.0-fpm php7.0-mcrypt php7.0-bz2 php7.0-gd php7.0-mysql php7.0-soap php7.0-cgi php7.0-gmp php7.0-odbc php7.0-sqlite3 php7.0-cli php7.0-imap php7.0-opcache php7.0-sybase php7.0-common php7.0-interbase php7.0-pgsql php7.0-tidy php7.0-curl php7.0-intl php7.0-phpdbg php7.0-xml php7.0-dba php7.0-json php7.0-pspell php7.0-xmlrpc php7.0-dev php7.0-ldap php7.0-readline php7.0-xsl php7.0-enchant php7.0-mbstring php7.0-recode php7.0-zip php-xdebug
+
+# configure xdebug
+echo "[Xdebug]\nzend_extension=\"/usr/lib/php/20151012/xdebug.so\"\nxdebug.remote_enable=1\nxdebug.remote_handler=dbgp\nxdebug.remote_mode=req\nxdebug.remote_host=127.0.0.1\nxdebug.remote_port=9000\nxdebug.max_nesting_level=300\n" >> /etc/php/7.0/apache2/php.ini
 
 # install gulp
 npm install gulp-cli -g
@@ -36,8 +39,8 @@ rm *.py
 cd ..
 git clone git@github.com:pixiucz/cli.git
 sudo -u $SUDO_USER sh -c "pipsi install --editable cli"
-# rm -rf cli
-# pipsi upgrade --editable cli
+rm -rf cli
+pipsi upgrade --editable cli
 cd dev-setup
 
 # set rights to /var/www
